01ca8c5a67df1da7b6c40ea8692c339ca4832d25
Hasani <abdul.kadir.hasani@basajans.com>
2017-03-08 00:01:23 +0700

    test ES6

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,9 +4,21 @@
 import React from "react";
 export class Home extends React.Component{
     render(){
+        /**
+         * keyword let berarti variabel tersebut hanya berlaku untuk
+         * block tertentu
+         * @type {string}
+         */
+        let content = "";
+        if(true){
+            content = <p>Hello !</p>;
+        }
         return(
             <div>
                 <p>In new Component</p>
+                {content}
+                {"Hello Rindu"}
+                { 5 == 45 ? "YES":"NO"}
             </div>
         );
     }
