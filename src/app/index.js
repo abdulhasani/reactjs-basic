ee64a0571e1c840628297a7e02356328456b7e72
Hasani <abdul.kadir.hasani@basajans.com>
2017-03-07 23:02:07 +0700

    add css with bootstrap and use className in React JS

--- src/app/index.js
+++ src/app/index.js
@@ -3,8 +3,12 @@ import { render } from "react-dom";
 class App extends React.Component{
     render(){
         return (
-            <div>
-                <h1>Hello, React JS Beatiful</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1>Hello, React JS Beatiful</h1>
+                    </div>
+                </div>
             </div>
         );
     }
