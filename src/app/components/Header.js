9cf1ef66c83c72d8024e4e2194f6eeddb9f567e5
Hasani <abdul.kadir.hasani@basajans.com>
2017-03-13 14:49:01 +0700

    fix example

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -7,18 +7,16 @@ import React from "react";
  * agar component pada class header
  * dapat digunakan pada class App pada file index.js
  */
-export class Header extends React.Component{
-    render() {
+export const Header = (props) => {
         return (
             <nav className="navbar navbar-default">
                 <div className="container">
                     <div className="navbar-header">
                         <ul className="nav navbar-nav">
-                            <li><a href="#">Home</a></li>
+                            <li><a href="#">{props.homeLink}</a></li>
                         </ul>
                     </div>
                 </div>
             </nav>
         );
-    }
 }
