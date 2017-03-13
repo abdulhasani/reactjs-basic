c5f14538e849273ba108dc7dd766b9e64601c971
Hasani <abdul.kadir.hasani@basajans.com>
2017-03-13 16:25:32 +0700

    function onGreet for popup

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,10 @@ import { render } from "react-dom";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 class App extends React.Component{
+    onGreet(){
+        alert("Hello!");
+    }
+
     render(){
         return (
             <div className="container">
@@ -13,7 +17,7 @@ class App extends React.Component{
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"Max"} initialAge={27}/>
+                        <Home name={"Max"} initialAge={27} greet={this.onGreet}/>
                     </div>
                 </div>
             </div>
